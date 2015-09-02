9266f18c3211a466a4a8e63c73b10ca479af9e35
michelcedric <michelcedric18@hotmail.com>
2015-09-02 14:16:35 +0200

    Initial commit
    Commit entire project with a test exemple

---
+++ TestUserImpersonation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace TestUserImpersonation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = "";
+            string domain = "";
+            string password = "";
+
+            using (UserImpersonation user = new UserImpersonation(login, domain, password))
+            {
+                if (user.ImpersonateValidUser())
+                {
+                    File.WriteAllText("test2.txt", "Voici mon texte");
+                    Console.WriteLine("File writed");
+                }
+                else
+                {
+                    Console.WriteLine("User not connected");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
